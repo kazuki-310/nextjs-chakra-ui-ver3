8e6d09abe0d0cec33abce11d620ef11062b0ad00
kazuki-310 <kazu.0310123@gmail.com>
2025-04-27 21:30:44 +0900

    first commit

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  endOfLine: 'lf',
+};
