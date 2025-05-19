dec6b911fa48ad02093b081b7a079c89dc089146
kazuki-310 <kazu.0310123@gmail.com>
2025-05-19 22:36:17 +0900

    feat(toaster): integrate toaster functionality in button click with promise handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Provider } from '@/components/ui/provider';
+import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,7 +15,10 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning>
       <body>
-        <Provider>{children}</Provider>
+        <Provider>
+          <Toaster />
+          {children}
+        </Provider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { HStack } from '@chakra-ui/react';
 import { DialogTest } from './dialog-test';
+import { toaster } from '@/components/ui/toaster';
 
 export default function Home() {
   return (
     <div>
       <HStack gap={4} p={4}>
         <div>test</div>
-        <Button>Click me</Button>
+        <Button
+          onClick={() => {
+            const randomPromise = new Promise<void>((resolve, reject) => {
+              setTimeout(() => {
+                Math.random() > 0.5 ? resolve() : reject(new Error('Something went wrong'));
+              }, 1000);
+            });
+            toaster.promise(randomPromise, {
+              success: {
+                title: 'Successfully uploaded!',
+                description: 'Looks great',
+              },
+              error: {
+                title: 'Upload failed',
+                description: 'Something wrong with the upload',
+              },
+              loading: { title: 'Uploading...', description: 'Please wait' },
+            });
+          }}
+        >
+          Click me
+        </Button>
         <Button>Click me</Button>
       </HStack>
 
